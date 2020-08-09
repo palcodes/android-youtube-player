6b6eecfc10f71d8086b2d511114fba3eec445efe
Aayush Pal <ap.aayushpal@gmail.com>
2020-08-09 14:19:24 +0530

    local setup

--- chromecast-receiver/js/main.js
+++ chromecast-receiver/js/main.js
@@ -1,44 +1,50 @@
-import SenderMessagesDispatcher from "./io/SenderMessagesDispatcher.js"
-import ChromecastCommunicationChannel from "./io/ChromecastCommunicationChannel.js"
-import YouTubePlayer from "./YouTubePlayer.js"
+import SenderMessagesDispatcher from "./io/SenderMessagesDispatcher.js";
+import ChromecastCommunicationChannel from "./io/ChromecastCommunicationChannel.js";
+import YouTubePlayer from "./YouTubePlayer.js";
 
-const namespace = "urn:x-cast:com.pierfrancescosoffritti.androidyoutubeplayer.chromecast.communication"
-let communicationConstants = {}
+const namespace =
+  "urn:x-cast:com.pierfrancescosoffritti.androidyoutubeplayer.chromecast.communication";
+let communicationConstants = {};
 
-const context = cast.framework.CastReceiverContext.getInstance()
+const context = cast.framework.CastReceiverContext.getInstance();
 
-const communicationChannel = new ChromecastCommunicationChannel(namespace)
-const youTubePlayer = new YouTubePlayer(communicationConstants, communicationChannel)
-const senderMessagesDispatcher = new SenderMessagesDispatcher(communicationConstants, { onInitMessageReceived, ...youTubePlayer.getActions() })
+const communicationChannel = new ChromecastCommunicationChannel(namespace);
+const youTubePlayer = new YouTubePlayer(
+  communicationConstants,
+  communicationChannel
+);
+const senderMessagesDispatcher = new SenderMessagesDispatcher(
+  communicationConstants,
+  { onInitMessageReceived, ...youTubePlayer.getActions() }
+);
 
-let isYouTubeIframeAPIReady = false
+let isYouTubeIframeAPIReady = false;
 
-context.addCustomMessageListener(namespace, senderMessagesDispatcher.onMessage)
-context.start()
+context.addCustomMessageListener(namespace, senderMessagesDispatcher.onMessage);
+context.start();
 
 function onInitMessageReceived(parsedCommunicationConstants) {
-    if(!isYouTubeIframeAPIReady) {
-        initCommunicationConstants(parsedCommunicationConstants)
-        loadYouTubeIFrameAPIs()
-    } else
-        youTubePlayer.restoreCommunication()
+  if (!isYouTubeIframeAPIReady) {
+    initCommunicationConstants(parsedCommunicationConstants);
+    loadYouTubeIFrameAPIs();
+  } else youTubePlayer.restoreCommunication();
 }
 
 function initCommunicationConstants(parsedCommunicationConstants) {
-    for (let key in parsedCommunicationConstants)
-        communicationConstants[key] = parsedCommunicationConstants[key]
+  for (let key in parsedCommunicationConstants)
+    communicationConstants[key] = parsedCommunicationConstants[key];
 }
 
 function loadYouTubeIFrameAPIs() {
-    const script = document.createElement('script')
-    script.src = "https://www.youtube.com/iframe_api"
-    document.getElementsByTagName('head')[0].appendChild(script)
+  const script = document.createElement("script");
+  script.src = "https://www.youtube.com/iframe_api";
+  document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 // called automatically by the IFrame APIs
 function onYouTubeIframeAPIReady() {
-    isYouTubeIframeAPIReady = true
-    youTubePlayer.initialize()
+  isYouTubeIframeAPIReady = true;
+  youTubePlayer.initialize();
 }
 
-window.main_onYouTubeIframeAPIReady = onYouTubeIframeAPIReady
+window.main_onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
